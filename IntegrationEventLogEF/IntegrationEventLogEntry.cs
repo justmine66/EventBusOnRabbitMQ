9055dc16c10d1d464839be5f38bd69deb22fd5fe
Administrator <Administrator@JUSTMINE-PC>
2017-07-03 13:15:47 +0800

    采用EF记录事件日志到sqlserver数据库。

---
+++ IntegrationEventLogEF/IntegrationEventLogEntry.cs
@@ -0,0 +1,27 @@
+﻿using EventBus.Events;
+using IntegrationEventLogEF.valueObjects;
+using Newtonsoft.Json;
+using System;
+
+namespace IntegrationEventLogEF
+{
+    public class IntegrationEventLogEntry
+    {
+        private IntegrationEventLogEntry() { }
+        public IntegrationEventLogEntry(IntegrationEvent @event)
+        {
+            EventId = @event.Id;
+            CreationTime = @event.CreationDate;
+            EventTypeName = @event.GetType().FullName;
+            Content = JsonConvert.SerializeObject(@event);
+            State = EventStateEnum.NotPublished;
+            TimesSent = 0;
+        }
+        public Guid EventId { get; private set; }
+        public string EventTypeName { get; private set; }
+        public EventStateEnum State { get; set; }
+        public int TimesSent { get; set; }
+        public DateTime CreationTime { get; private set; }
+        public string Content { get; private set; }
+    }
+}
---
+++ IntegrationEventLogEF/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationEventLogEF.Services
+{
+    public interface IIntegrationEventLogService
+    {
+        Task SaveEventAsync(IntegrationEvent @event, DbTransaction transaction);
+        Task MarkEventAsPublishedAsync(IntegrationEvent @event);
+    }
+}
